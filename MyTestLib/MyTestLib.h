38c7e33677db7f84433e9193d82dd5550532a734
Ronny Antony <ronny.antony@zcocorporation.com>
2021-06-07 19:33:07 +0530

    Initial commit

---
+++ MyTestLib/MyTestLib.h
@@ -0,0 +1,18 @@
+//
+//  MyTestLib.h
+//  MyTestLib
+//
+//  Created by Ronny Antony on 07/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyTestLib.
+FOUNDATION_EXPORT double MyTestLibVersionNumber;
+
+//! Project version string for MyTestLib.
+FOUNDATION_EXPORT const unsigned char MyTestLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyTestLib/PublicHeader.h>
+
+
---
+++ MyTestLibExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTestLibExample
+//
+//  Created by Ronny Antony on 07/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
